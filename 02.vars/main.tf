aa009329d84f378f444082f9fb28db4198ab0318
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-27 20:51:48 +0530

    updating ./learning-terraform

---
+++ 02.vars/main.tf
@@ -0,0 +1,23 @@
+variable "a" {
+  default = 100
+}
+
+output "a" {
+  value = var.a
+}
+
+output "x" {
+  value = "Values of a=${var.a}"
+}
+
+output "values" {
+  value = "Values of a=${var.a}, b=${var.b}, c=${var.c}, d=${var.d}"
+}
+
+variable "b" {}
+
+variable "c" {}
+# -var c=400
+
+variable "d" {}
+# TF_VAR_d=500

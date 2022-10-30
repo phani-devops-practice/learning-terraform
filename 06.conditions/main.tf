9b08cbb78acd4d4188e75d85e132ec66e65471c2
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-30 15:12:25 +0530

    updating ./learning-terraform

--- 06.conditions/main.tf
+++ 06.conditions/main.tf
@@ -1,8 +1,8 @@
 #conditions is all about picking up the value form the arguments
 
 resource "aws_instance" "web" {
-  ami                    = "ami-0408d6aa07d74894b"
-  instance_type          = var.instance_type == null ? "t3.micro" : var.instance_type
+  ami                    = data.aws_ami.example.image_id
+  instance_type          = local.instance_type
 
   tags = {
     Name = "terraform"
@@ -11,6 +11,17 @@ resource "aws_instance" "web" {
 
 variable "instance_type" {
   default = null
+}
+
+locals {
+  instance_type = var.instance_type == null ? "t3.micro" : var.instance_type
+}
+
+data "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "ansible-inbuilt"
+  owners           = ["self"]
 }
 
 #Here we can override the instance by using the command : terraform apply -auto-approve -var instance_type = "t2.micro"

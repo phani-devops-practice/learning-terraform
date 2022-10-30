997b1748e1c1be7994fec23a95adfba2827bbdd7
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-30 09:24:08 +0530

    updating ./learning-terraform

--- 04.loops/approches/approach-03/main.tf
+++ 04.loops/approches/approach-03/main.tf
@@ -1,13 +1,15 @@
 resource "aws_instance" "web" {
   count                  = length(var.instance_type)
   ami                    = "ami-0408d6aa07d74894b"
-  instance_type          = var.instance_type[count.index+1]
+  instance_type          = var.instance_type[count.index]
 
   tags = {
-    Name = "terraform"
+    Name = "terraform-${count.index+1}"
   }
 }
 
 variable "instance_type" {
   default = ["t3.micro", "t2.micro"]
 }
+
+

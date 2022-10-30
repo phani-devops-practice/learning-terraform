c19b7149325d4cfea3beb2ae63e0b2efdc2476ab
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-30 13:05:26 +0530

    updating ./learning-terraform

---
+++ 07.provisioners/main.tf
@@ -0,0 +1,54 @@
+#provisioners are sub-block in resources
+
+#these provisioners are used to execute certain tasks after the resource creation
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0408d6aa07d74894b"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name = "terraform"
+  }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = DevOps321
+      host     = self.public_ip
+    }
+
+    inline = [
+      "uname"
+    ]
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "vpc-0ea60a247fdfddb3a"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "TCP"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+

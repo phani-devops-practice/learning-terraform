54358275ba46236aa8f4564f7f1d3e31a22024be
devops-learner-phani <109220162+devops-learner-phani@users.noreply.github.com>
2022-10-30 09:02:29 +0530

    updating ./learning-terraform

--- 04.loops/approches/approach-02/main.tf
+++ 04.loops/approches/approach-02/main.tf
@@ -1,8 +1,5 @@
-variable "name_tag" {
-  default = 2
-}
-
 module "ec2" {
+  count = 2
   source = "./module"
   name_tag = "terraform-${count.index+1}"
 }
--- 04.loops/approches/approach-02/module/main.tf
+++ 04.loops/approches/approach-02/module/main.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "web" {
-  count                  = 2
   ami                    = "ami-0408d6aa07d74894b"
   instance_type          = "t3.micro"
 
